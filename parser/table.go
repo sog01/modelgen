4776beea50da75c1626dd401092c7a2304a639ae
sog01 <abdullahsog3@gmail.com>
2023-08-11 00:18:24 +0700

    feat: done create table parser

--- parser/table.go
+++ parser/table.go
@@ -7,19 +7,6 @@ import (
 	"github.com/sog01/modelgen/types"
 )
 
-/*
-CREATE TABLE IF NOT EXISTS tasks (
-    task_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    start_date DATE,
-    due_date DATE,
-    status TINYINT NOT NULL,
-    priority TINYINT NOT NULL,
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)  ENGINE=INNODB;
-*/
-
 type Table struct {
 	Type    types.DDLType
 	Table   string
@@ -30,23 +17,52 @@ type Column struct {
 	Name       string
 	Type       string
 	Identifier bool
-	Nullable   bool
+	NotNull    bool
 }
 
 func ParseTable(s string) (Table, error) {
 	s = strings.TrimSpace(s)
+	s = strings.ToLower(s)
 	splitted := strings.Split(s, "\n")
 	if len(splitted) == 0 {
 		return Table{}, errors.New("empty string")
 	}
 	switch typ := parseDDLType(splitted[0]); typ {
 	case types.CreateTable:
-		return Table{
+		t := Table{
 			Type:  typ,
 			Table: parseCreateTable(splitted[0]),
-		}, nil
+		}
+
+		for _, s := range splitted[1 : len(splitted)-1] {
+			t.Columns = append(t.Columns, parseColumn(s))
+		}
+		return t, nil
 	}
 	return Table{}, nil
+}
+
+func parseColumn(s string) *Column {
+	s = strings.TrimSpace(s)
+	splitted := strings.Split(s, " ")
+	c := Column{
+		Name:       splitted[0],
+		Type:       sanitizeColumnType(splitted[1]),
+		Identifier: strings.Contains(s, "primary key"),
+		NotNull:    strings.Contains(s, "not null"),
+	}
+
+	return &c
+}
+
+func sanitizeColumnType(s string) string {
+	s = strings.ReplaceAll(s, ",", "")
+	bracketIndex := strings.Index(s, "(")
+	if bracketIndex > -1 {
+		return s[:bracketIndex]
+	}
+
+	return s
 }
 
 func parseCreateTable(s string) string {
